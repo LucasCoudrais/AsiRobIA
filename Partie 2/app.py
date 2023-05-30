d549d3283f0310f6858a3bb7c644e7a54a82abad
LucasCoudrais <lucas.coudrais@gmail.com>
2023-05-30 10:15:47 +0200

    début api flask

--- Partie 2/app.py
+++ Partie 2/app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/hello', methods=['GET'])
+def hello():
+    message = {'message': 'Hello, World!'}
+    return jsonify(message)
 
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = read_users()
+    return jsonify(users)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    user = request.get_json()
+    users = read_users()
+    users.append(user)
+    write_users(users)
+    return jsonify(user), 201
+
+def read_users():
+    with open('data/users.json', 'r') as file:
+        users = json.load(file)
+    return users
+
+def write_users(users):
+    with open('data/users.json', 'w') as file:
+        json.dump(users, file)
 
 if __name__ == '__main__':
     app.run()
