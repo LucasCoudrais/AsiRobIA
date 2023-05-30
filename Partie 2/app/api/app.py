2b619e2e08d02407eebb241e1317c21528de39f7
LucasCoudrais <lucas.coudrais@gmail.com>
2023-05-30 17:08:33 +0200

    ajout streaming vidéo mais vidéo hébergé dans l'api

--- Partie 2/app/api/app.py
+++ Partie 2/app/api/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from controllers.tempController import TempController
 from controllers.proximityController import ProximityController
 from controllers.interuptorController import InteruptorController
+from controllers.streamController import StreamController
 
 app = Flask(__name__)
 
 temp_controller = TempController()
 proximity_controller = ProximityController()
 interuptor_controller = InteruptorController()
+stream_controller = StreamController()
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -43,6 +45,22 @@ def get_interuptors():
 @app.route('/interuptor/<int:interuptor_id>', methods=['PUT'])
 def update_interuptor(interuptor_id):
     return interuptor_controller.update_interuptor(interuptor_id, request)
+
+
+
+@app.route('/streamCamera', methods=['GET'])
+def stream_camera():
+    video_path = 'video.mp4'  # Chemin vers votre fichier vidéo
+
+    def generate():
+        with open(video_path, 'rb') as video_file:
+            while True:
+                video_data = video_file.read(1024)
+                if not video_data:
+                    break
+                yield video_data
+
+    return Response(generate(), mimetype='video/mp4')
 
 if __name__ == '__main__':
     app.run(debug=True)
